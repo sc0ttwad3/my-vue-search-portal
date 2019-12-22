fbfc8a3bd89cbbb53754832a5095365294f6c8d5
Scott Wade <scottwade.net@live.com>
2019-12-21 23:33:37 -0500

    Remove core-js and useBuiltins: usage instead

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: [
+    [
+      "@vue/cli-plugin-babel/preset",
+      {
+        "preset-env": {
+          useBuiltIns: "usage"
+        }
+      }
+    ]
+  ]
 };
